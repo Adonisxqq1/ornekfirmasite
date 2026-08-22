b9966e625480b7fb28d714b5fbfdc659c52b0bea
Adonisxqq1 <153298871+Adonisxqq1@users.noreply.github.com>
2026-08-22 15:08:43 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -12,6 +12,8 @@ const UPLOAD_DIR = path.join(__dirname, "public", "uploads");
 
 fs.mkdirSync(UPLOAD_DIR, { recursive: true });
 
+app.set("trust proxy", 1);
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
